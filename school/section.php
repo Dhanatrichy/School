8eb0c3e07cdc4410773c83187139fb7aa40d13a4
Gowthyn <gowthamnandakumar.aha@gmail.com>
2022-07-14 16:16:55 +0530

    class and section management done by Gowtham

--- school/section.php
+++ school/section.php
@@ -1,12 +1,12 @@
 <?php
 include('includes/database.php');
 if (!isset($_SESSION['admin_id'])) {
-    header('location: index.php');
+    header('location:');
 }
 $action = 'submit';
 $id = base64_decode($_GET['id']);
 if ($_GET['action'] == "edit" && isset($_GET['id'])) {
-    $max = "select * from tbl_class where id='" . $id . "'";
+    $max = "select * from tbl_section where id='" . $id . "'";
     $max2 = mysqli_query($db, $max);
     $max3 = mysqli_fetch_assoc($max2);
     extract($max3);
@@ -19,7 +19,7 @@ if ($_GET['action'] == "edit" && isset($_GET['id'])) {
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Classes | LMS</title>
+        <title>Section | LMS</title>
 
         <?php include_once 'includes/header.php'; ?>
     </head>
@@ -36,12 +36,12 @@ if ($_GET['action'] == "edit" && isset($_GET['id'])) {
                     <div class="container-fluid">
                         <div class="row mb-2">
                             <div class="col-sm-6">
-                                <h1 class="m-0">Add / View Class</h1>
+                                <h1 class="m-0">Add / View Section</h1>
                             </div><!-- /.col -->
                             <div class="col-sm-6">
                                 <ol class="breadcrumb float-sm-right">
                                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                    <li class="breadcrumb-item active">Class</li>
+                                    <li class="breadcrumb-item active">Section</li>
                                 </ol>
                             </div><!-- /.col -->
                         </div><!-- /.row -->
@@ -58,19 +58,19 @@ if ($_GET['action'] == "edit" && isset($_GET['id'])) {
                                 <!-- general form elements -->
                                 <div class="card card-primary">
                                     <div class="card-header">
-                                        <h3 class="card-title"><i class="fas fa-plus-square"></i> Add Class</h3>
+                                        <h3 class="card-title"><i class="fas fa-plus-square"></i> Add Section</h3>
                                     </div>
                                     <!-- /.card-header -->
                                     <!-- form start -->
-                                    <form method="post" name="class_form" id="class_form">
+                                    <form method="post" name="section_form" id="section_form">
                                         <input type="hidden" name="action" value="<?php echo $action; ?>">
                                         <input type="hidden" name="id" value="<?php echo $id; ?>">
                                         <div class="card-body">
                                             <div class="row">
                                                 <div class="col-md-12">
                                                     <div class="form-group">
-                                                        <label for="name">Class Name</label>
-                                                        <input type="text" class="form-control" id="name" name="name" placeholder="Class Name" value="<?php echo $name; ?>">
+                                                        <label for="name">Section Name</label>
+                                                        <input type="text" class="form-control" id="name" name="name" placeholder="Section Name" value="<?php echo $name; ?>">
                                                     </div>
                                                 </div>
                                             </div>
@@ -84,20 +84,20 @@ if ($_GET['action'] == "edit" && isset($_GET['id'])) {
 
                                 <div class="card card-info">
                                     <div class="card-header">
-                                        <h3 class="card-title"><i class="fas fa-file-alt"></i> View Class</h3>
+                                        <h3 class="card-title"><i class="fas fa-file-alt"></i> View Section</h3>
                                     </div>
                                     <div class="card-body">
                                         <table id="example1" class="table table-bordered table-striped">
                                             <thead>
                                                 <tr>
-                                                    <th>Class Name</th>
+                                                    <th>Section Name</th>
                                                     <th>Status</th>
                                                     <th>Action</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
                                                 <?php
-                                                $query = "SELECT * FROM tbl_class ORDER BY name ASC";
+                                                $query = "SELECT * FROM tbl_section ORDER BY name ASC";
                                                 $result = mysqli_query($db, $query);
                                                 while ($row = mysqli_fetch_assoc($result)) {
                                                     ?>
@@ -105,15 +105,15 @@ if ($_GET['action'] == "edit" && isset($_GET['id'])) {
                                                         <td><?php echo $row['name']; ?></td>
                                                         <td>
                                                             <?php if ($row['status'] == 'active') { ?>
-                                                            <a href="javascript:void(0);" class="status_<?php echo $row['id']; ?>"><span class="badge bg-success" onclick="change_status(<?php echo $row['id']; ?>, 'inactive', 'modules/class/request.php')">Active</span></a>
+                                                            <a href="javascript:void(0);" class="status_<?php echo $row['id']; ?>"><span class="badge bg-success" onclick="change_status(<?php echo $row['id']; ?>, 'inactive', 'modules/section/request.php')">Active</span></a>
                                                             <?php } else { ?>
-                                                            <a href="javascript:void(0);" class="status_<?php echo $row['id']; ?>"><span class="badge bg-danger" onclick="change_status(<?php echo $row['id']; ?>, 'active', 'modules/class/request.php')">Inactive</span></a>
+                                                            <a href="javascript:void(0);" class="status_<?php echo $row['id']; ?>"><span class="badge bg-danger" onclick="change_status(<?php echo $row['id']; ?>, 'active', 'modules/section/request.php')">Inactive</span></a>
                                                             <?php } ?>		
                                                         </td>
                                                         <td >
                                                             <div class="btn-group btn-group-sm">
-                                                                <a href="class.php?id=<?php echo base64_encode($row['id']); ?>&action=edit" class="btn btn-info"><i class="fas fa-eye"></i></a>
-                                                                <a href="javascript:void(0);" class="btn btn-danger"><i onclick="delete_detail(<?php echo $row['id']; ?>, 'modules/class/request.php')" class="fas fa-trash"></i></a>
+                                                                <a href="section.php?id=<?php echo base64_encode($row['id']); ?>&action=edit" class="btn btn-info"><i class="fas fa-eye"></i></a>
+                                                                <a href="javascript:void(0);" class="btn btn-danger"><i onclick="delete_detail(<?php echo $row['id']; ?>, 'modules/section/request.php')" class="fas fa-trash"></i></a>
                                                             </div>
                                                         </td>
                                                     </tr>
