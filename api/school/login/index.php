3bdf20b92a88523c6afd97553e28a1985370c51d
Gowthyn <gowthamnandakumar.aha@gmail.com>
2022-08-09 15:40:32 +0530

    done

--- api/school/login/index.php
+++ api/school/login/index.php
@@ -20,7 +20,7 @@ if (isset($_GET["action"])) {
         $query = "select * from tbl_school where email='" . $email . "' and password='" . $password . "' and status='active'";
         $run = mysqli_query($db, $query);
         $count = mysqli_num_rows($run);
-        if ($count > 0) {
+        if ($count  > 0) {
             while ($row = mysqli_fetch_assoc($run)) {
                 $arr[] = $row;
             }
